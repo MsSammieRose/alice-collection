5660d2014061e577618d2382f3d538d919ad4192
sam <scampbell134@gmail.com>
2024-08-18 17:04:43 -0400

    mobile navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -13,52 +13,30 @@ import {
 } from "@chakra-ui/react";
 
 const Navigation: React.FC = () => {
+  const menuItems = ["Disney", "Lewis Carrol", "Treasures"];
   return (
     <>
       <VStack width="100vw" backgroundColor="paper">
-        <HStack spacing="24px" marginTop={4}>
+        <Stack direction={["column", "row"]} spacing="24px" marginTop={4}>
           <Menu>
-            <MenuButton
-              margin={2}
-              height={3}
-              width="100px"
-              fontFamily={"heading"}
-              _hover={{
-                borderBottomColor: "grey",
-                borderBottomWidth: "2px",
-                fontWeight: "bold",
-              }}
-            >
-              Disney
-            </MenuButton>
-            <MenuButton
-              height={3}
-              margin={2}
-              width="100px"
-              fontFamily={"heading"}
-              _hover={{
-                borderBottomColor: "grey",
-                borderBottomWidth: "2px",
-                fontWeight: "bold",
-              }}
-            >
-              Lewis Carrol
-            </MenuButton>
-            <MenuButton
-              height={3}
-              margin={2}
-              width="100px"
-              fontFamily={"heading"}
-              _hover={{
-                borderBottomColor: "grey",
-                borderBottomWidth: "2px",
-                fontWeight: "bold",
-              }}
-            >
-              Treasures
-            </MenuButton>
+            {menuItems.map((item) => (
+              <MenuButton
+                height={[2, 3]}
+                margin={[0, 2]}
+                width="100px"
+                fontFamily={"heading"}
+                _hover={{
+                  borderBottomColor: "grey",
+                  borderBottomWidth: "2px",
+                  fontWeight: "bold",
+                }}
+                key={item}
+              >
+                {item}
+              </MenuButton>
+            ))}
           </Menu>
-        </HStack>
+        </Stack>
 
         <Stack direction={["row"]}>
           <Image
@@ -67,6 +45,8 @@ const Navigation: React.FC = () => {
             src="/cards.jpg"
             alt="the cards from Alice in Wonderland"
             borderRadius="10px"
+            border="4px solid"
+            borderColor="#f0e5d1"
             style={{ rotate: "30deg", zIndex: 1 }}
           />
 
@@ -89,6 +69,8 @@ const Navigation: React.FC = () => {
             alt="the tea party from Alice in Wonderland"
             borderRadius="10px"
             style={{ rotate: "-20deg", zIndex: 2 }}
+            border="4px solid"
+            borderColor="#f0e5d1"
             marginTop={20}
             marginLeft={-20}
           />
