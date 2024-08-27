5f37ede8e23e47b86872f8659ffd6eed6dff5688
sam <scampbell134@gmail.com>
2024-08-27 12:04:17 -0400

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/main";
 import Article from "./views/article";
 import theme from "./theme";
@@ -9,13 +9,13 @@ import "@fontsource/goudy-bookletter-1911/400.css";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Router>
+    <HashRouter>
       <MainLayout>
         <Routes>
           <Route path="" element={<Intro />} />
           <Route path="/article" element={<Article />} />
         </Routes>
       </MainLayout>
-    </Router>
+    </HashRouter>
   </ChakraProvider>
 );
